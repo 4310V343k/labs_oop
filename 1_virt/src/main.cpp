b155336baf363a5fd7a758ef7270e6b67efbf940
4310V343k <56449763+4310V343k@users.noreply.github.com>
2026-03-10 14:20:07 +0300

    1 1_virt figure2 is not needed

--- 1_virt/src/main.cpp
+++ 1_virt/src/main.cpp
@@ -21,18 +21,10 @@ int main()
     Figure figure(hdc, 200, 200, true, RGB(0, 255, 0));
     figure.Show();
 
-    Figure figure2(hdc, 300, 200, true, RGB(0, 0, 255));
-    Point *figure_as_point = &figure2;
-    figure_as_point->Show();
-
     // управление объектами
     point.Control();
 
     figure.Control();
-
-    // управление объектом производного класса через базовый класс
-    // демонстрирует, что при вызове MoveTo из базового класса вызывается ранее (статически, при компиляции) связанный Point::MoveTo
-    figure_as_point->Control();
 
     return 0;
 }
